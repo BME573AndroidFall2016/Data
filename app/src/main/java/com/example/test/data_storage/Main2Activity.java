e48cf7f0e1b5735cdc06b133fd6583ce74190513
Jacky <xiejacky16@gmail.com>
2016-11-29 13:00:37 -0500

    Second Activity - Primary get and put methods

--- app/src/main/java/com/example/test/data_storage/Main2Activity.java
+++ app/src/main/java/com/example/test/data_storage/Main2Activity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -17,6 +18,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -24,63 +26,60 @@ import java.util.List;
 public class Main2Activity extends AppCompatActivity {
 
     ListView LV1;
-    ArrayList<test> test1 = new ArrayList<test>();
-    Button B1;
-    boolean buttonpress = false;
-    int clickcount = 0;
-    int[] G1 = {clickcount};
+    //ArrayList<test2> Q1 = new ArrayList<test2>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
         LV1 = (ListView) findViewById(R.id.listview2);
-        Button B1 = (Button)findViewById(R.id.test);
     }
 
-
-
-    public void testwrite(View v) {
-        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
-        if (buttonpress = false)
+    public void testwrite(View v){
+        ArrayList<test2> write = new ArrayList<test2>();   // Create an arraylist object that can be read by listview and stored as object into file
+        int type = 1;                               // Type of data (ECG - 1, etc) *essentially placeholder
+        double[] dset = {1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,1.11};     // Set of 10 doubles from sensor
+        long milli = System.currentTimeMillis();    // Get time of system when button pressed
+        long buffer = 100;                          // Create a time "buffer" that allows for the timestamps to differ by a certain amount of time
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss:SS");    // Create a format that the timestamp is im
+        Date result = new Date(milli);                                          // Casts the system time in millisecs to a Date object
+        test2[] tt2 = new test2[dset.length];                                   // Initialize object array of data class with lenth of incoming data array
+        for (int i = 0; i < dset.length; i++)
         {
-            test t1 = new test(G1,currentDateTimeString);
-            test1.add(t1);
-            buttonpress = true;
+            tt2[i] = new test2(type,dset[i],sdf.format(result));    // Set data into the object array tagged with timestamp
+            milli += buffer;                                        // Add buffer to the initial time
+            result = new Date(milli);                               // Update the timestamp with the buffered value
         }
-        else {};
+        for (int i = 0; i < dset.length; i++)
+        {
+            write.add(tt2[i]);     // Add the array of data objects to the arraylist object
+        }
         try {
-            FileOutputStream fout1 = openFileOutput("test-test.txt", MODE_PRIVATE);
-            ObjectOutputStream oos1 = new ObjectOutputStream(fout1);
-            oos1.writeObject(test1);
-            oos1.close();
+            FileOutputStream fout = openFileOutput("data.txt", MODE_APPEND);
+            ObjectOutputStream oos = new ObjectOutputStream(fout);
+            oos.writeObject(write);
+            oos.close();
             Toast.makeText(getBaseContext(), "File Saved!", Toast.LENGTH_SHORT).show();
-            B1.isEnabled();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public void testread(View v) {
-        ArrayList<test> Y1;
+        ArrayList<test2> read;
         try {
-            FileInputStream fin1 = openFileInput("test-test.txt");
-            ObjectInputStream ois1 = new ObjectInputStream(fin1);
-            Y1 = (ArrayList<test>) (ois1.readObject());
-            ois1.close();
-            fin1.close();
-            ArrayAdapter testAdapter = new ArrayAdapter<test>(this, android.R.layout.simple_list_item_1, Y1);
-            LV1.setAdapter(testAdapter);
+            FileInputStream fin = openFileInput("data.txt");
+            ObjectInputStream ois = new ObjectInputStream(fin);
+            read = (ArrayList<test2>) (ois.readObject());
+            ois.close();
+            fin.close();
+            ArrayAdapter testAdapter = new ArrayAdapter<test2>(this, android.R.layout.simple_list_item_1, read);    // Create an adapter to display the data to listview
+            LV1.setAdapter(testAdapter);        // Set adapter to listview
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void testadd(View v){
-        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
-        clickcount = clickcount + 1;
-        int[] G2 = {clickcount};
-        test t2 = new test(G2,currentDateTimeString);
-        test1.add(t2);
-    }
+
 }
--- app/src/main/java/com/example/test/data_storage/test2.java
+++ app/src/main/java/com/example/test/data_storage/test2.java
@@ -1,17 +1,15 @@
 package com.example.test.data_storage;
 
-import android.support.v7.app.AppCompatActivity;
-import android.text.format.Time;
 
 import java.io.Serializable;
 import java.util.Arrays;
 
-public class test implements Serializable {
+public class test2 implements Serializable{
     int Datatype;
-    double[] D1;
-    String[] Timestamp;
+    double D1;
+    String Timestamp;
 
-    public test(int datatype, double[] i1, String[] S)
+    public test2(int datatype, double i1, String S)
     {
         Datatype = datatype;
         D1 = i1;
@@ -23,12 +21,12 @@ public class test implements Serializable {
     {
         String item;
 
-        item = Arrays.toString(D1) + " - " +  Arrays.toString(Timestamp);
+        item = D1 + " - " +  Timestamp;
 
         return item;
     }
 
-    public void putData(int datatype, double[] sensorvalues, String[] times)
+    public void putData(int datatype, double sensorvalues, String times)
     {
         Datatype = datatype;
         D1 = sensorvalues;
